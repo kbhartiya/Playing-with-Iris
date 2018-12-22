5ed55a7088cfa451737d91f68ae6e40474b7cadc
Kartik Bhartiya <kbhartiya83@gmail.com>
2018-12-22 13:37:58 +0530

    Add files via upload

--- kmeans_iris.py
+++ kmeans_iris.py
@@ -7,33 +7,39 @@ def init(data,k):
     
     # The centroids
 	centroids = np.mat(np.zeros((k,n)))
-
+	#print(np.shape(centroids))
     # Create random centroids (get min, max attribute values, randomize in that range)
 	for j in range(n):
 		min_j = min(data[:,j])
 		range_j = float(max(data[:,j]) - min_j)
 		centroids[:,j] = min_j + range_j*np.random.rand(k, 1)
-
+	return centroids
+	
+import math	
 def euclidean_distance(data_point1, data_point2):
+		#data_point2.reshape((1,4))
+		#print(data_point1," ",data_point2," \n")
 		if len(data_point1) != len(data_point2) :
 			raise ValueError('feature length not matching')
 		else:
 			distance = 0
-			for x in range(len(data_point1)):
-				distance += pow((data_point1[x] - data_point2[x]), 2)
+			#print(data_point1[0,1]," ",data_point2[0,1],"\n")
+			for x in range(np.shape(data_point2)[1]):
+				distance += pow((data_point1[0,x] - data_point2[0,x]), 2)
+				print(distance)
 			return math.sqrt(distance)
 
 def cluster(data, k):
 	# Number of rows in dataset
-	print(np.shape(data)[0])
+	#print(np.shape(data))
 	m = np.shape(data)[0]
 
 	# Hold the instance cluster assignments
 	cluster_assignments = np.mat(np.zeros((m, 2)))
-
+	#print(np.shape(cluster_assignments))
 	# Initialize centroids
 	centroids = init(data, k)
-    
+	#print(np.shape(centroids))
 	# Preserve original centroids
 	cents_orig = centroids.copy()
     
@@ -54,8 +60,8 @@ def cluster(data, k):
 
 			# Calculate distances
 			for j in range(k):
-
-				dist_ji = euclidean_distance(cents[j,:], data[i,:])
+				#print(centroids[j].shape,"  ",np.reshape(data[i],(1,4)).shape)
+				dist_ji = euclidean_distance(centroids[j], np.reshape(data[i],(1,4)))
 				if dist_ji < min_dist:
 					min_dist = dist_ji
 					min_index = j
@@ -70,13 +76,13 @@ def cluster(data, k):
 		# Update centroid location
 		for cent in range(k):
 			points = data[np.nonzero(cluster_assignments[:,0].A==cent)[0]]
-			cents[cent,:] = np.mean(points, axis=0)
+			centroids[cent,:] = np.mean(points, axis=0)
 
 		# Count iterations
 		num_iter += 1
 
 	# Return important stuff when done
-	return cents, cluster_assignments, num_iter, cents_orig	
+	return centroids, cluster_assignments, num_iter, cents_orig	
 
 def main():
 	#load the csv_file as a pandas dataframe. 
@@ -85,7 +91,7 @@ def main():
 	data = pd.read_csv(iris_csv_file,names=names)
 	#print(data)
 	feature_columns = ['SepalLength_cm', 'SepalWidth_cm', 'PetalLength_cm','PetalWidth_cm']
-	X = data[feature_columns].as_matrix
+	X = data[feature_columns].values
 	#Drop the labels as it is an unsupervised learning problem.
 	data.drop(['label'],axis=1,inplace=True) 
 	#print(data.shape)
